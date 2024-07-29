025310d28d2be0a47489a21ca7b2f0d377dc07c2
Komal Sharma <ks11201605@gmail.com>
2024-07-29 23:40:04 +0530

    2nd cooment

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
