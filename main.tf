1666f80d647cf1a13d68947ae4d5a6a487718643
Komal Sharma <ks11201605@gmail.com>
2024-07-29 23:33:30 +0530

    2nd update

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
